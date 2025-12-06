b8e8c3a649f92f23a186b0eb4b8656193d2dba34
abba97-h <adiakite545@gmail.com>
2025-12-06 19:07:34 +0000

    changement

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { inject as analyticsInject } from "@vercel/analytics";
+
+analyticsInject();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
